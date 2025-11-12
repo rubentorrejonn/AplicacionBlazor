b9436c4f14f381193aa8a11b32b076c5101bd845
Practicas Soporte <practicas.soporte@icp.es>
2025-11-12 16:09:57 +0100

    PA_ASIGNAR_STOCK aun por terminar

---
+++ SQL/PROCEDIMIENTOS ALMACENADOS/PA_ASIGNAR_STOCK.sql
@@ -0,0 +1,160 @@
+USE FCT_OCT_1
+GO
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+	NOMBRE DEL PROCEDIMIENTO:	PA_ASIGNAR_STOCK
+	FECHA DE CREACIÓN: 		12/11/2025
+	AUTOR:				Rubén
+	VSS:				E:\DevOps\PRACTICAS\FCT_PRACTICAS\2025\FCT_OCT_1\SQL\PROCEDIMIENTOS ALMACENADOS
+	USO:				##VISUAL##
+
+	FUNCIONAMIENTO:			Comprueba que hay stock de esa referencia en los palets y los deja en un estado "OCUPADO"
+
+	PARAMETROS:			(OPCIONAL)
+		PARAMETRO1 		INPUT	EXPLICACION
+		PARAMETRO2 		OUTPUT	EXPLICACION
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------
+--	FECHA DE MODIFICACIÓN:
+--	AUTOR:
+--	EXPLICACIÓN:	
+------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+CREATE PROCEDURE PA_ASIGNAR_STOCK
+	
+	@PETICION	INT,
+
+	@INVOKER	INT,		-- ESTE PARÁMETRO LO DEBEN TENER TODOS LOS PAS
+	@USUARIO	VARCHAR(12),	-- ESTE PARÁMETRO LO DEBEN TENER TODOS LOS PAS
+	@CULTURA	VARCHAR(5),
+
+	@RETCODE	INT OUTPUT, --DEFINICIÓN OBLIGATORIA
+	@MENSAJE	VARCHAR(1000)	OUTPUT	--DEFINICIÓN OBLIGATORIA
+
+AS
+
+BEGIN TRY
+	--DECLARACION DE VARIABLES 
+
+	DECLARE @N_TRANS		INT = 0	 --NUMERO DE TRANSACCIONES ACTIVAS	(@@TRANCOUNT)
+	SET @N_TRANS = @@TRANCOUNT
+	
+	DECLARE @REFERENCIA VARCHAR(30);
+	DECLARE @CANTIDAD_REQUERIDA INT;
+	DECLARE @CANTIDAD_ASIGNADA INT;
+	DECLARE @PALET INT;
+
+	--COMPROBACIONES
+	IF @PETICION IS NULL
+	BEGIN
+		SET @MENSAJE = 'EL PARAMETRO ' + CAST(@PETICION AS VARCHAR) + ' ES REQUERIDO.'
+		SET @RETCODE = 1
+	END
+	IF @CANTIDAD_ASIGNADA < @CANTIDAD_REQUERIDA
+	BEGIN
+		SET @MENSAJE = 'NO HAY SUFICIENTE STOCK DISPONIBLE PARA LA REFERENCIA ' + @REFERENCIA
+		SET @RETCODE = 1
+	END
+	----------------------------------------------------------------------------------------------------------------------------------------------
+	IF @N_TRANS = 0						-- Si hay una transacción por encima no hacemos nada
+	BEGIN
+		BEGIN TRANSACTION TR_NOMBRE_TRANSACTION
+	END
+	----------------------------------------------------------------------------------------------------------------------------------------------
+
+	--OPERACIONES
+	--CURSOR PARA RECORRER LINEAS DE SALIDA
+	DECLARE CUR_SALIDA CURSOR FOR
+	SELECT 
+		REFERENCIA, CANTIDAD
+	FROM 
+		ORDEN_SALIDA_LIN
+	WHERE 
+		PETICION = @PETICION
+	
+	OPEN CUR_SALIDA;
+	FETCH NEXT FROM CUR_SALIDA INTO @REFERENCIA, @CANTIDAD_REQUERIDA
+	--MIENTRAS QUE SEA [ 0 = EXITOSA ]
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		SET @CANTIDAD_ASIGNADA = 0;
+		--CURSOR PARA RECORRER PALETS
+		DECLARE CUR_PALETS CURSOR FOR
+		SELECT
+			PALET, CANTIDAD
+		FROM 
+			PALETS
+		WHERE 
+			REFERENCIA = @REFERENCIA AND ESTADO = '1'
+		ORDER BY
+			PALET
+	END
+
+		
+	
+	----------------------------------------------------------------------------------------------------------------------------------------------
+	IF @N_TRANS = 0						-- Si hay una transacción por encima no hacemos nada
+	BEGIN
+		COMMIT TRANSACTION TR_NOMBRE_TRANSACTION
+	END
+	----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+	SET @MENSAJE = 'El proceso se ha realizado correctamente.'
+	SET @RETCODE = 0
+	RETURN @RETCODE
+END TRY
+BEGIN CATCH
+	----------------------------------------------------------------------------------------------------------------------------------------------
+	IF @N_TRANS = 0 AND @@TRANCOUNT > 0				-- Si hay una transacción por encima no hacemos nada
+	BEGIN
+		ROLLBACK TRANSACTION TR_NOMBRE_TRANSACTION
+	END
+	----------------------------------------------------------------------------------------------------------------------------------------------
+	IF @MENSAJE = '' 
+	BEGIN
+		SET  @MENSAJE = ERROR_MESSAGE()
+	END
+	
+	SET @RETCODE = -1
+		
+	RETURN @RETCODE
+END CATCH
+
+	SET @RETCODE = -1		
+	RETURN @RETCODE
+
+
+
+
+
+
+/*----------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+------------------------------               PRUEBAS              ------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+
+
+DECLARE @RETCODE	INT
+DECLARE @PARAMETRO1	VARCHAR(10)
+DECLARE @PARAMETRO2	INT
+DECLARE @PARAMETRO3	VARCHAR(20)
+DECLARE @MENSAJE	VARCHAR(1000)
+
+SET @MENSAJE 		= ''
+
+EXEC @RETCODE = PA_XXXX @PARAMETRO1, @PARAMETRO2, @PARAMETRO3 OUTPUT, @MENSAJE OUTPUT
+
+PRINT 'RETCODE:	' 	+ ISNULL(CAST(@RETCODE AS VARCHAR(10)), 'NULO')
+PRINT 'MENSAJE:	' 	+ ISNULL(@MENSAJE, 'NULO')
+PRINT 'PARAMETRO3''	+ ISNULL(@PARAMETRO3, 'NULO')
+
+
+
+------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+------------------------------             FIN PRUEBAS            ------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------*/
